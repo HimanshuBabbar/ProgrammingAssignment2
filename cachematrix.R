09c8eb52b7cc1e10d62b8a7236de6704da05cd45
HimanshuBabbar <babbar.himanshu@gmail.com>
2014-11-21 00:33:48 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,49 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(varMatrix = matrix()){
+        #Add checks for non ivertible matrices
+  
+        matrixInverse <- NULL
+        
+        getInverse <- function(){
+              return(matrixInverse)
+        }
+        
+        setInverse <- function(inverse){
+              matrixInverse <<- inverse
+        }   
+  
+        getValue <- function(){
+              return(varMatrix)
+        }
+        
+        #setvalue <- function(varMatrix){
+         #     varMatrix <- varMatrix
+        #}
+        
+        invisible(list(getValue = getValue, setInverseM = setInverse,  getInverse = getInverse))
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix){
+        
+        inverseMatrix <- matrix$getInverse()
+        
+        if(!is.null(inverseMatrix)){  # if inverse was already cached (not NULL) ...
+              message("The inverse of this matrix has been calculated and is :")
+              return(inverseMatrix)
+        }
+        else {                        # find the inverse of matrix
+              originalMatrix <- matrix$getValue()
+              inverseMatrix <- solve(originalMatrix)    # check for singular matrices
+              
+              matrix$setInverse(inverseMatrix)
+              
+              return(inverseMatrix)
+          
+        }
+        
 }
